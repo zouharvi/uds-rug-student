79a01d1ced39d6215aa1ecf5c5b11f48044acbe2
Vilém Zouhar <vilem.zouhar@gmail.com>
2021-02-19 16:06:55 +0100

    nn project separate

--- neural_networks/project/process.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cat data/{chtb_0223,phoenix_0001,pri_0016,wsj_1681}.gold_conll > data/sample.conll
-grep -v -E "^#.*$" data/sample.conll | awk '/^$/ {print "*"} NF {print $3"\t"$4"\t"$5}' > data/sample.tsv
